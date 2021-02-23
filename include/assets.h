844f663a449d9acf7b6185312d22e2c7eca4b369
Gabriel Lima <gabrielgsdlima@gmail.com>
2021-02-23 20:42:06 -0300

    Adds some assests that will be used

---
+++ include/assets.h
@@ -0,0 +1,17 @@
+#ifndef ASSETS_H
+#define ASSETS_H
+
+// Players
+#define PLAYER_X 0x274C
+#define PLAYER_O 0x2b55
+
+// Characters controls
+#define BOLD    "\033[1m"
+#define RESET   "\033[m"
+
+// Characters colors
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define BLUE    "\033[34m"
+
+#endif
