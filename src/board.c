bcb421b2a434684db92ab25855fa14290537f4dc
Gabriel Lima <gabrielgsdlima@gmail.com>
2021-02-24 05:51:21 -0300

    Modified to show the board in the center of the screen

--- src/board.c
+++ src/board.c
@@ -8,14 +8,17 @@ void show_board(wchar_t array[])
 {
     printf("%s", BOLD);
 
+    printf("\t\t\t\t\t");
     (array[0] == '1') ? printf(" %lc  |", array[0]) : printf(" %lc |", array[0]);
     (array[1] == '2') ? printf(" %lc  |", array[1]) : printf(" %lc |", array[1]);
     (array[2] == '3') ? printf(" %lc  ", array[2]) : printf(" %lc ", array[2]);
-    printf("\n----|----|----\n");
+    printf("\n\t\t\t\t\t----|----|----\n");
+    printf("\t\t\t\t\t");
     (array[3] == '4') ? printf(" %lc  |", array[3]) : printf(" %lc |", array[3]);
     (array[4] == '5') ? printf(" %lc  |", array[4]) : printf(" %lc |", array[4]);
     (array[5] == '6') ? printf(" %lc  ", array[5]) : printf(" %lc ", array[5]);
-    printf("\n----|----|----\n");
+    printf("\n\t\t\t\t\t----|----|----\n");
+    printf("\t\t\t\t\t");
     (array[6] == '7') ? printf(" %lc  |", array[6]) : printf(" %lc |", array[6]);
     (array[7] == '8') ? printf(" %lc  |", array[7]) : printf(" %lc |", array[7]);
     (array[8] == '9') ? printf(" %lc  ", array[8]) : printf(" %lc ", array[8]);
