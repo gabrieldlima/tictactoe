48c92260762672b53cd7f8efb66d556156053dc9
Gabriel Lima <gabrielgsdlima@gmail.com>
2021-02-24 07:06:25 -0300

    Added some more assets

--- include/assets.h
+++ include/assets.h
@@ -5,13 +5,30 @@
 #define PLAYER_X 0x274C
 #define PLAYER_O 0x2b55
 
+// Objects
+#define TROPHY          0x1F3C6
+#define BLACK_SQUARE    0x25FE
+#define WHITE_SQUARE    0x25FD
+#define ORANGE_STAR     0x2734
+
 // Characters controls
 #define BOLD    "\033[1m"
 #define RESET   "\033[m"
 
 // Characters colors
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define BLUE    "\033[34m"
+#define RED             "\033[31m"
+#define GREEN           "\033[32m"
+#define ORANGE          "\033[33m"
+#define BLUE            "\033[34m"
+#define PINK            "\033[35m"
+#define AQUAMARINE      "\033[36m"
+
+// Characters colors + Bold
+#define RED_BOLD            "\033[1;31m"
+#define GREEN_BOLD          "\033[1;32m"
+#define ORANGE_BOLD         "\033[1;33m"
+#define BLUE_BOLD           "\033[1;34m"
+#define PINK_BOLD           "\033[1;35m"
+#define AQUAMARINE_BOLD     "\033[1;36m"
 
 #endif
