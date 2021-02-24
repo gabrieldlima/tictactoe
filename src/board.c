87324fb4c826bc32e939a1395fb194f6f8f322ab
Gabriel Lima <gabrielgsdlima@gmail.com>
2021-02-24 07:23:49 -0300

    Added header for the board

--- src/board.c
+++ src/board.c
@@ -6,7 +6,25 @@
 
 void show_board(wchar_t array[])
 {
-    printf("%s", BOLD);
+    for (int i = 0; i <= 102; ++i)
+    {
+        if (i < 51)
+        {
+            printf("%lc", BLACK_SQUARE);
+        }
+        else if (i == 51)
+        {
+            putchar('\n');
+            printf("\t\t\t\t\t%s%lc TIC TAC TOE %lc  %s", ORANGE_BOLD, ORANGE_STAR, ORANGE_STAR, RESET);
+            putchar('\n');
+        }
+        else if (i > 51)
+        {
+            printf("%lc", WHITE_SQUARE);    
+        }
+    }
+
+    printf("%s\n", BOLD);
 
     printf("\t\t\t\t\t");
     (array[0] == '1') ? printf(" %lc  |", array[0]) : printf(" %lc |", array[0]);
@@ -23,5 +41,5 @@ void show_board(wchar_t array[])
     (array[7] == '8') ? printf(" %lc  |", array[7]) : printf(" %lc |", array[7]);
     (array[8] == '9') ? printf(" %lc  ", array[8]) : printf(" %lc ", array[8]);
 
-    printf("%s", RESET);
+    printf("%s\n", RESET);
 }
