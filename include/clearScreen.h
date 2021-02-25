8ca9bc415664a4c981c61c6fd64c71449c7260ab
Gabriel Lima <gabrielgsdlima@gmail.com>
2021-02-25 07:09:45 -0300

    Added function to clear the screen

---
+++ include/clearScreen.h
@@ -0,0 +1,6 @@
+#ifndef CLEARSCREEN_H
+#define CLEARSCREEN_H
+
+void clear_screen(void);
+
+#endif
---
+++ src/clearScreen.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+#include "../include/clearScreen.h"
+
+void clear_screen(void)
+{
+    system("clear");
+}
