73185567f1d89239a06263f84b4873046d7d0f98
Gabriel Lima <gabrielgsdlima@gmail.com>
2021-05-10 06:56:20 -0300

    Removed unnecessary assets

--- src/assets.h
+++ src/assets.h
@@ -2,35 +2,18 @@
 #define ASSETS_H
 
 // Players
-#define X 0x274C
-#define O 0x2B55
+#define X   0x274C
+#define O   0x2B55
 
 // Objects
-#define TROPHY          0x1F3C6
-#define BLACK_SQUARE    0x25FE
-#define WHITE_SQUARE    0x25FD
-#define ORANGE_STAR     0x2734
-#define PERSON          0x1F464
-#define COMPUTER        0x1F5A5
-
-// Characters controls
-#define BOLD    "\033[1m"
-#define RESET   "\033[m"
-
-// Characters colors
-#define RED             "\033[31m"
-#define GREEN           "\033[32m"
-#define ORANGE          "\033[33m"
-#define BLUE            "\033[34m"
-#define PINK            "\033[35m"
-#define AQUAMARINE      "\033[36m"
+#define TROPHY  0x1F3C6
 
 // Characters colors + Bold
-#define RED_BOLD            "\033[1;31m"
-#define GREEN_BOLD          "\033[1;32m"
-#define ORANGE_BOLD         "\033[1;33m"
-#define BLUE_BOLD           "\033[1;34m"
-#define PINK_BOLD           "\033[1;35m"
-#define AQUAMARINE_BOLD     "\033[1;36m"
+#define RED_BOLD    "\033[1;31m"
+#define WHITE_BOLD  "\033[1;37m"
+#define RESET       "\033[m"
+
+// Background
+#define WHITE_BKG   "\033[7;37m"
 
 #endif
