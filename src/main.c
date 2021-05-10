e72be78248380d139a7c9e6a4aabb73606ac6ea5
Gabriel Lima <gabrielgsdlima@gmail.com>
2021-05-10 06:10:36 -0300

    Refactoring

--- src/main.c
+++ src/main.c
@@ -5,63 +5,68 @@
 #include <wchar.h>
 #include <locale.h>
 
-#include "assets.h"
 #include "board.h"
-
-#define TRUE  1
-#define FALSE 0
+#include "assets.h"
 
 int handleInput(void);
 void clearScreen(void);
 
+
 int main(void)
 {   
-    /*=-=-=-=-=-
-    GAME SETUP()
-    =-=-=-=-=-*/
+    /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    GAME SETUP
+    =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
     wchar_t player1;
     wchar_t player2;
-    wchar_t board[9];
     wchar_t winner;
+    wchar_t board[9];
     
     int player1Pos;
     int player2Pos;
     int round;
-    int match = 0;
+    int matches = 0;
     
     setlocale(LC_ALL, "en_US.utf-8");
 
 
-    /*=-=-=-=-=
-    GAME LOOP()
-    =-=-=-=-=*/
+    /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // GAME LOOP
+    =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
     while (1)
     {
         player1 = X;
         player2 = O;
-        player1Pos = 1;
-        player2Pos = 1;
-        winner = '.';
-        round = 1;
-        match++;
+        winner = ' ';
         wchar_t board[9] = {
         '1', '2', '3',
         '4', '5', '6', 
         '7', '8', '9'
         };
 
+        player1Pos = 1;
+        player2Pos = 1;
+        round = 1;
+        matches += 1;
+
+        
+        //----------------------------------------------------------------------------------------------------
         // The maximum number of iterations in the loop is 9 rounds, according to the number of positions on the board
-        for (round; round <= 9; ++round)
+        for (round; round <= 9; round++)
         {
+            //----------------------------------------------------------------------------------------------------
             // Updates the player's screen
             clearScreen();
-            showBoard(board, round, match);
+            showBoard(board, round, matches);
 
+
+            //----------------------------------------------------------------------------------------------------
             // Asks the player for an input
-            // printf("%sEnter a position %lc\n»»» ", BOLD, (round % 2 != 0) ? X : O);
-            printf("%sPlayer %c, enter a position:\n»»» ", BOLD, (round % 2 != 0) ? '1' : '2');
+            printf("%sEnter a position: ", WHITE_BOLD);
             (round % 2 != 0) ? (player1Pos = handleInput()) : (player2Pos = handleInput());
 
+
+            //----------------------------------------------------------------------------------------------------
             // Checks if the input is within the allowed values
             if ((player1Pos < 1 || player1Pos > 9) || (player2Pos < 1 || player2Pos > 9))
             {
@@ -69,6 +74,9 @@ int main(void)
                 sleep(1.5);
                 --round;
             }
+            
+            
+            //----------------------------------------------------------------------------------------------------
             // Checks whether the chosen position is free on the board
             else if (((round % 2 != 0) && !(isdigit(board[player1Pos - 1]))) || ((round % 2 == 0) && !(isdigit(board[player2Pos - 1]))))
             {
@@ -76,6 +84,9 @@ int main(void)
                 sleep(1.5);
                 --round;
             }
+            
+            
+            //----------------------------------------------------------------------------------------------------
             // If the entry is correct, place the piece in the chosen position on the board
             else
             {
@@ -85,12 +96,14 @@ int main(void)
                     board[player2Pos - 1] = player2;
             }   
 
+
+            //----------------------------------------------------------------------------------------------------
             // Checks if there is a victory condition and closes the loop
             if (round >= 5)
             {
                 for (int i = 0; i < 9; i++)
                 {
-                    // Horizontal
+                    // Horizontal check
                     if (i == 0 || i == 3 || i == 6)
                     {
                         if (board[i] == X && board[i+1] == X && board[i+2] == X)
@@ -102,7 +115,7 @@ int main(void)
                             winner = O;
                         }  
                     }
-                    // Vertical
+                    // Vertical check
                     if (i == 0 || i == 1 || i == 2)
                     {
                         if (board[i] == X && board[i+3] == X && board[i+6] == X)
@@ -114,7 +127,7 @@ int main(void)
                             winner = O;
                         }
                     }
-                    // Diagonal
+                    // Diagonal check
                     if (i == 0 || i == 2)
                     {
                         if (i == 0)
@@ -144,18 +157,25 @@ int main(void)
             }
 
             if (winner == X || winner == O)
-            {
-                clearScreen();
-                showBoard(board, round, match);
-                printf("Winner %lc\n", winner);
                 break;
-            }
-
         }
 
-
-        // Game Loop end
-        // break;
+        //----------------------------------------------------------------------------------------------------
+        // Shows the victorious player on the screen
+        if (winner == X || winner == O)
+        {
+            clearScreen();
+            showBoard(board, round, matches);
+            printf("%sPLAYER %lc WON THE GAME!%s\n", WHITE_BOLD, winner, RESET);
+        }
+        else
+        {
+            clearScreen();
+            showBoard(board, round, matches);
+            printf("%sDRAW!%s\n", WHITE_BOLD, RESET);
+        }
+        
+        break;
     }
 
     return 0;
